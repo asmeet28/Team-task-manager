8286ca78db8ba9d56da1ea4d8d528f572e89019c
asmeetky <asmeetky@gmail.com>
2026-05-01 12:41:24 +0530

    fix vite config allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: 'all',
     host: '0.0.0.0',
-    port: 8080
+    port: 8080,
+    allowedHosts: [
+      'all',
+      'sunny-caring-production-4f3c.up.railway.app'
+    ]
   },
   server: {
     host: '0.0.0.0',
