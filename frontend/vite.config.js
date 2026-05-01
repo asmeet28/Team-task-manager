5cd93d0a854a2bba1e95a9761aad7e175ad80438
asmeetky <asmeetky@gmail.com>
2026-05-01 12:34:30 +0530

    fix vite preview host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['all'],
-    host: true,
+    allowedHosts: 'all',
+    host: '0.0.0.0',
     port: 8080
   },
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173
   }
 })
